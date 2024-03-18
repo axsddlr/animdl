86a86b31e1245bd36636434cb3efc8de3c5c7e56
Andre Saddler <dresadd09691@gmail.com>
2024-03-18 14:11:24 -0400

    update to gogoanime3.co

--- animdl/core/config/__init__.py
+++ animdl/core/config/__init__.py
@@ -64,7 +64,7 @@ DEFAULT_CONFIG = {
         "animtime": "https://animtime.com/",
         "crunchyroll": "http://www.crunchyroll.com/",
         "kawaiifu": "https://kawaiifu.com/",
-        "gogoanime": "https://gogoanime3.net/",
+        "gogoanime": "https://gogoanime3.co/",
         "haho": "https://haho.moe/",
         "hentaistream": "https://hstream.moe/",
         "kamyroll_api": "https://kamyroll.herokuapp.com/",
